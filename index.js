1f2ce99ba027d01b9f7234b31be53e346d6a1176
Erick Castille <ecastille924@gmail.com>
2021-07-05 18:04:22 -0400

    commit tree ec2104f8618430dfb788be82bf5ec71b6cf4a9a7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(foundName => 
+       foundName.toUpperCase() == name.toUpperCase() )
+    }
+
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(foundName =>
+        foundName.charAt(0) == name.charAt(0))
+}
+
+function matchName(drivers, name) {
+    return drivers.filter( function(foundName){
+        if (foundName.name === name)
+        return foundName
+    })
+}
